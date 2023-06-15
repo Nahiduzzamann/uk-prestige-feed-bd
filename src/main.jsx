f13efc7b8acbb40e4a3a0266bb9167f372daa81e
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-06-15 11:57:05 +0600

    project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Main from './layout/Main';
+import Home from './pages/Home/Home/Home';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+     
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className=''>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar =()=>{
+    return(
+        <div>
+            <h1>This is Navbar</h1>
+        </div>
+    )
+}
+export default Navbar;
