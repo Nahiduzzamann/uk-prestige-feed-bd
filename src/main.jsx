133c73ef0ece425878e8e93db6ee3bb4c11699d6
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-06-15 14:39:39 +0600

    added logo nav top

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import Main from './layout/Main';
 import Home from './pages/Home/Home/Home';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Navbar =()=>{
-    return(
+const Navbar = () => {
+    return (
         <div>
-            <h1>This is Navbar</h1>
+            <div className="w-full bg-gradient-to-r from-red-900 via-red-600 to-blue-600">
+                <div className='container mx-auto flex items-center justify-between'>
+                    <img className='h-28' src="https://i.ibb.co/8m2cvHw/Prestige-Feed-Industry-Ltd-removebg-preview.png" alt="" />
+                    <h1 className='text-3xl font-bold italic text-[#1A2753]'>Balanced feed, Trusted Company</h1>
+
+                </div>
+            </div>
+
         </div>
     )
 }
