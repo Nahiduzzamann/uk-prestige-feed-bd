68a99e3533a6638f0c483b1a3497ecdf0bf75459
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-06-17 12:04:41 +0600

    update navbar and banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,25 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div>
-            <div className="carousel w-full h-1/2">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://www.ekoplon.pl/extensions/module/products/images/2020/07/08/lp-prestiz-_3.jpg" className="w-full h-1/2" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
+        <div className=''>
+            <Carousel
+                autoPlay={true}
+                infiniteLoop={true}
+                showArrows={true}
+                showStatus={false}
+                showIndicators={true}
+                showThumbs={false}
+                interval={2000}
+                centerMode={true}
+                centerSlidePercentage={80}
+            >
+                <div className=''>
+                    <img className=' lg:h-[750px]' src="https://cdn.shopify.com/s/files/1/0508/4767/8644/articles/Responsible_Dog_Ownership_1200x.jpg?v=1663345594" alt="Slider Image 1" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Dog Feed</h2>
                     </div>
                 </div>
-                <div id="slide2" className="carousel-item relative w-full h-1/2">
-                    <img src="https://cdn.shopify.com/s/files/1/0508/4767/8644/articles/Responsible_Dog_Ownership_1200x.jpg?v=1663345594" className="w-full h-1/2" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
+                <div>
+                    <img className='lg:h-[750px]' src="https://www.ekoplon.pl/extensions/module/products/images/2020/07/08/lp-prestiz-_3.jpg" alt="Slider Image 2" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Boiler Feed</h2>
                     </div>
                 </div>
-            </div>
+                <div>
+                    <img className='lg:h-[750px]' src="https://www.beefmagazine.com/sites/beefmagazine.com/files/4-05-22%20cattle%20on%20feed%20GettyImages-1160802367.jpg" alt="Slider Image 2" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Cattle Feed</h2>
+                    </div>
+                </div>
+                <div>
+                    <img className='lg:h-[750px]' src="https://i.ytimg.com/vi/2vtBvVFQDWQ/maxresdefault.jpg" alt="Slider Image 2" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Layer Feed</h2>
+                    </div>
+                </div>
+                <div>
+                    <img className='lg:h-[750px]' src="https://i.ytimg.com/vi/U2sgIACyLPQ/maxresdefault.jpg" alt="Slider Image 2" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Sonali Feed</h2>
+                    </div>
+                </div>
+                <div>
+                    <img className='lg:h-[750px]' src="https://www.aces.edu/wp-content/uploads/2021/05/shutterstock_1606325479-scaled.jpg" alt="Slider Image 2" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Fish Feed</h2>
+                    </div>
+                </div>
+                <div>
+                    <img className='lg:h-[750px]' src="https://i5.walmartimages.com/asr/5ee78c28-d2cc-4575-a373-8c75444f9340.a53365569a9ce9b9e0a438b2c42d2e50.jpeg" alt="Slider Image 2" />
+                    <div className="legend text-base">
+                        <h2 className='text-3xl'>Dry Cat Feed</h2>
+                    </div>
+                </div>
+            </Carousel>
         </div>
-    )
-}
+
+    );
+};
+
 export default Banner;
+
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,80 +1,186 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Banner from '../../Home/Banner/Banner';
 
 const Navbar = () => {
-    return (
-        <div className="w-full bg-gradient-to-r from-red-900 via-red-600 to-blue-600">
-            <div>
-                <div className='container mx-auto flex items-center justify-between'>
-                    <div className='h-28 '>
-                        <img className='h-28 hidden lg:flex' src="https://i.ibb.co/8m2cvHw/Prestige-Feed-Industry-Ltd-removebg-preview.png" alt="" />
-                    </div>
-                    <h1 className='text-xl md:text-4xl font-bold italic text-[#1A2753]'>Balanced Feed, Trusted Company</h1>
-                </div>
-            </div>
-            {/* navigations section */}
-            <nav className=''>
-                <div className='navbar container mx-auto'>
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost lg:hidden ">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                                </svg>
+  const [isOpen, setIsOpen] = useState(false);
 
-                            </label>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-[#1A2753] rounded-box w-52 text-white">
-                                <li><Link to='/'>Home</Link></li>
-                                <li>
-                                    <a>Products</a>
-                                    <ul className="p-2">
-                                        <li><Link to='/'>Prestige Cattle Feed</Link></li>
-                                        <li><Link to='/'>Prestige Boiler Feed</Link></li>
-                                        <li><Link to='/'>Prestige Layer Feed</Link></li>
-                                        <li><Link to='/'>Prestige Sonali Feed</Link></li>
-                                        <li><Link to='/'>Prestige Fish Feed</Link></li>
-                                        <li><Link to='/'>Prestige Dry Dog Feed</Link></li>
-                                        <li><Link to='/'>Prestige Dry Cat Feed</Link></li>
-                                    </ul>
-                                </li>
-                                <li><Link to='/'>Services</Link></li>
-                                <li><Link to='/'>About Us</Link></li>
-                                <li><Link to='/'>Contact Us</Link></li>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-                            </ul>
-                        </div>
-                        <a className=" normal-case text-xl"></a>
-                    </div>
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1 bg-gradient-to-r from-red-900 via-red-600 to-blue-600 border-l-4 border-blue-500 rounded-r-full">
-                            <li><Link className='text-white hover:text-gray-300 lg:text-2xl ml-4 mr-5' to='/'>Home</Link></li>
-                            <li tabIndex={0}>
-                                <details>
-                                    <summary className='text-white hover:text-gray-300 lg:text-2xl mr-5'>Products</summary>
-                                    <ul className="p-2 bg-[#1A2753]">
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Cattle Feed</Link></li>
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Boiler Feed</Link></li>
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Layer Feed</Link></li>
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Sonali Feed</Link></li>
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Fish Feed</Link></li>
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Dry Dog Feed</Link></li>
-                                        <li><Link className='text-white hover:text-gray-300 lg:text-xl' to='/'>Prestige Dry Cat Feed</Link></li>
-                                    </ul>
-                                </details>
-                            </li>
-                            <li><Link className='text-white hover:text-gray-300 lg:text-2xl  mr-5' to='/'>Services</Link></li>
-                            <li><Link className='text-white hover:text-gray-300 lg:text-2xl  mr-5' to='/'>About Us</Link></li>
-                            <li><Link className='text-white hover:text-gray-300 lg:text-2xl  mr-5' to='/'>Contact Us</Link></li>
-                        </ul>
-                    </div>
-                    <div className="navbar-end h-28">
-                        <Link to='/'>
-                            <img className='h-28 flex lg:hidden' src="https://i.ibb.co/8m2cvHw/Prestige-Feed-Industry-Ltd-removebg-preview.png" alt="" />
-                        </Link>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div className="w-full bg-gradient-to-r from-red-900 via-red-600 to-blue-600">
+      <div>
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="h-28">
+            <img
+              className="h-28 hidden lg:flex"
+              src="https://i.ibb.co/8m2cvHw/Prestige-Feed-Industry-Ltd-removebg-preview.png"
+              alt=""
+            />
+          </div>
+          <h1 className="text-xl md:text-4xl font-bold italic text-[#1A2753]">
+            Balanced Feed, Trusted Company
+          </h1>
         </div>
-    )
-}
+      </div>
+      {/* Navigations section */}
+      <nav className="">
+        <div className="navbar container mx-auto">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <button
+                tabIndex={0}
+                className="btn btn-ghost lg:hidden"
+                onClick={toggleMenu}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </button>
+              {isOpen && (
+                <ul className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-[#1A2753] rounded-box w-52 text-white absolute z-50">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <a>Products</a>
+                    <ul className="p-2">
+                      <li>
+                        <Link to="/">Prestige Cattle Feed</Link>
+                      </li>
+                      <li>
+                        <Link to="/">Prestige Boiler Feed</Link>
+                      </li>
+                      <li>
+                        <Link to="/">Prestige Layer Feed</Link>
+                      </li>
+                      <li>
+                        <Link to="/">Prestige Sonali Feed</Link>
+                      </li>
+                      <li>
+                        <Link to="/">Prestige Fish Feed</Link>
+                      </li>
+                      <li>
+                        <Link to="/">Prestige Dry Dog Feed</Link>
+                      </li>
+                      <li>
+                        <Link to="/">Prestige Dry Cat Feed</Link>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <Link to="/">Services</Link>
+                  </li>
+                  <li>
+                    <Link to="/">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Contact Us</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+            <a className="normal-case text-xl"></a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 bg-gradient-to-r from-red-900 via-red-600 to-blue-600 border-l-4 border-blue-500 rounded-r-full">
+              <li>
+                <Link
+                  className="text-white hover:text-gray-300 lg:text-2xl ml-4 mr-5"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li tabIndex={0}>
+                <details>
+                  <summary className="text-white hover:text-gray-300 lg:text-2xl mr-5">
+                    Products
+                  </summary>
+                  <ul className="p-2 bg-[#1A2753] absolute z-50 ">
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Cattle Feed
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Boiler Feed
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Layer Feed
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Sonali Feed
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Fish Feed
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Dry Dog Feed
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="text-white hover:text-gray-300 lg:text-xl" to="/">
+                        Prestige Dry Cat Feed
+                      </Link>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <Link className="text-white hover:text-gray-300 lg:text-2xl  mr-5" to="/">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white hover:text-gray-300 lg:text-2xl  mr-5" to="/">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link className="text-white hover:text-gray-300 lg:text-2xl  mr-5" to="/">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end h-28">
+            <Link to="/">
+              <img
+                className="h-28 flex lg:hidden"
+                src="https://i.ibb.co/8m2cvHw/Prestige-Feed-Industry-Ltd-removebg-preview.png"
+                alt=""
+              />
+            </Link>
+          </div>
+        </div>
+      </nav>
+      {/* Slider Component */}
+      <Banner></Banner>
+    </div>
+  );
+};
+
 export default Navbar;
